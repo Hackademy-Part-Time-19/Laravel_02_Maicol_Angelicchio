3bcac836a0ef3f189023223e027b07bc92b3f778
Maicol Angelicchio <maicol.angelicchio.91@gmail.com>
2024-01-30 19:27:31 +0100

    esercizio 3 30/01/2024

--- progetto_25_01_24/resources/views/Articoli.blade.php
+++ progetto_25_01_24/resources/views/Articoli.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Articoli</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-<body>
-    <x-navbar/>
+<x-main>
+    <x-slot:title>Articoli</x-slot>
     <h1>ARTICOLI</h1>
     <div class="container mt-5">
         <div class="row">
@@ -16,6 +8,4 @@
             @endforeach
         </div>
     </div>
-
-</body>
-</html>
+</x-main>
