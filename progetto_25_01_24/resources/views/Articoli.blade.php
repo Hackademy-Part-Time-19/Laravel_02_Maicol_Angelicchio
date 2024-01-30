225ca9ba3681bd09106afde6af4bc5bd9a2b70c7
Maicol Angelicchio <maicol.angelicchio.91@gmail.com>
2024-01-30 19:41:43 +0100

    esercizio 5 30/01/2024

--- progetto_25_01_24/resources/views/Articoli.blade.php
+++ progetto_25_01_24/resources/views/Articoli.blade.php
@@ -4,7 +4,10 @@
     <div class="container mt-5">
         <div class="row">
             @foreach($articoli as $indice => $articolo)
-                <x-card :titolo="$articolo['titolo']" :categoria="$articolo['categoria']" :indice="$indice"/>
+                <x-card
+                :titolo="$articolo['titolo']"
+                :categoria="$articolo['categoria']"
+                :indice="$indice"/>
             @endforeach
         </div>
     </div>
